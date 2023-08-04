3f4c645b594d19a4d4cab67512f0ec7279b95bbf
Randy Concepcion <randy.concepcion@gmail.com>
2023-08-03 23:57:28 -0400

    Create endpoint to create, read, and delete users

--- backend/backend/app.py
+++ backend/backend/app.py
@@ -1,4 +1,8 @@
-from flask import Flask
+from flask import (
+    Flask,
+    request,
+    jsonify,
+)
 from flask_sqlalchemy import SQLAlchemy
 
 
@@ -11,7 +15,6 @@ db = SQLAlchemy(app)
 
 
 class Users(db.Model):
-    # primary_key makes it so that this value is unique and can be used to identify this record
     id = db.Column("student_id", db.Integer, primary_key=True)
     username = db.Column(db.String(24))
     email = db.Column(db.String(64))
@@ -23,9 +26,66 @@ class Users(db.Model):
         self.pwd = pwd
 
 
-@app.route("/")
-def index():
-    return "Hello, world!"
+@app.route("/api/users", methods=["GET", "POST", "DELETE"])
+def users():
+    method = request.method
+
+    if method.lower() == "get":
+        users = Users.query.all()
+
+        # Get all values from database
+        return jsonify(
+            [
+                {
+                    "id": user.id,
+                    "username": user.username,
+                    "email": user.email,
+                    "password": user.pwd,
+                }
+                for user in users
+            ]
+        )
+
+    elif method.lower() == "post":
+        try:
+            username = request.json["username"]
+            email = request.json["email"]
+            pwd = request.json["pwd"]
+
+            if username and pwd and email:
+                try:
+                    user = Users(username, email, pwd)
+                    db.session.add(user)
+                    db.session.commit()
+
+                    return jsonify({"success": True})
+
+                except Exception as e:
+                    return {"error": e}
+
+        except Exception:
+            jsonify({"error": "Invalid form"})
+
+    elif method.lower() == "delete":
+        try:
+            uid = request.json["id"]
+
+            if uid:
+                try:
+                    user = Users.query.get(uid)
+                    db.session.delete(user)
+                    db.session.commit()
+
+                    return jsonify({"success": True})
+
+                except Exception as e:
+                    return jsonify({"error": e})
+
+            else:
+                return jsonify({"error": "Invalid form"})
+
+        except Exception:
+            return jsonify({"error": "m"})
 
 
 if __name__ == "__main__":
