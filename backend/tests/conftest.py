87cf576acbb6e08a707a41410d88344b7c94709f
Randy Concepcion <randy.concepcion@gmail.com>
2023-08-11 01:33:42 -0400

    Update conftest.py to import pytest

--- backend/tests/conftest.py
+++ backend/tests/conftest.py
@@ -1,4 +1,33 @@
-import os
-import sys
+import pytest  # noqa F401
+from backend import create_app, db
+from backend.models import Users
 
-sys.path.insert(1, os.getcwd() + "/backend/")
+
+@pytest.fixture(scope="module")
+def test_client():
+    flask_app = create_app()
+
+    with flask_app.test_client() as testing_client:
+        with flask_app.app_context():
+            yield testing_client
+
+
+@pytest.fixture(scope="module")
+def init_database(test_client):
+    db.create_all()
+
+    default_user = Users(
+        username="bobloblaw", email="bob.loblaw@lawblog.com", pwd="12345"
+    )
+    second_user = Users(
+        username="michaelscott", email="michael.scott@dundermiffline.com", pwd="paper"
+    )
+
+    db.session.add(default_user)
+    db.session.add(second_user)
+
+    db.session.commit()
+
+    yield
+
+    db.drop_all()
