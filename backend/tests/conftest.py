bead4bbe46696c647ae552c4d652e3bd8c233d10
Randy Concepcion <randy.concepcion@gmail.com>
2023-08-14 17:51:02 -0400

    Fix: Disable CSRF for test client app

--- backend/tests/conftest.py
+++ backend/tests/conftest.py
@@ -6,6 +6,8 @@ from backend.models import Users
 @pytest.fixture(scope="module")
 def test_client():
     flask_app = create_app()
+    flask_app.config["TESTING"] = True
+    flask_app.config["WTF_CSRF_METHODS"] = []
 
     with flask_app.test_client() as testing_client:
         with flask_app.app_context():
