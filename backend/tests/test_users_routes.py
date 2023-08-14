f4f291e5ae92320a5070108c0d5b9a2ec6a9e23d
Randy Concepcion <randy.concepcion@gmail.com>
2023-08-14 01:00:59 -0400

    Add unit test for user success

--- backend/tests/test_users_routes.py
+++ backend/tests/test_users_routes.py
@@ -32,7 +32,7 @@ class TestUsersEndpoint:
         assert response.status_code == 400
         assert b"error" in response.data
 
-    def test_post_null_values_returns_exception(self):
+    def test_db_session_add_user_returns_invalid_form_response(self):
         post_json = {
             "username": "bobloblaw",
             "email": "bob.loblaw@lawblog.com",
@@ -50,6 +50,22 @@ class TestUsersEndpoint:
 
         assert response.status_code == 400
         assert b"error" in response.data
+
+    def test_post_null_values_returns_exception(self):
+        post_json = {
+            "username": "bobloblaw",
+            "email": None,
+            "pwd": None,
+        }
+
+        response = self.test_client.post(
+            self.endpoint,
+            content_type="application/json",
+            json=post_json,
+        )
+
+        assert response.status_code == 400
+        assert b"Invalid form" in response.data
 
     def test_post_valid_user_data_returns_success(self):
         post_json = {
