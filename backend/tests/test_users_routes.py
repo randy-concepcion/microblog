eeee58a2878d1007f480153404bd9d05f38f34e5
Randy Concepcion <randy.concepcion@gmail.com>
2023-08-14 00:50:01 -0400

    Add test case for valid user data for users POST returns success & fix if/else in POST

--- backend/tests/test_users_routes.py
+++ backend/tests/test_users_routes.py
@@ -50,3 +50,19 @@ class TestUsersEndpoint:
 
         assert response.status_code == 400
         assert b"error" in response.data
+
+    def test_post_valid_user_data_returns_success(self):
+        post_json = {
+            "username": "bobloblaw",
+            "email": "bob.loblaw@lawblog.com",
+            "pwd": "12345",
+        }
+
+        response = self.test_client.post(
+            self.endpoint,
+            content_type="application/json",
+            json=post_json,
+        )
+
+        assert response.status_code == 200
+        assert b"success" in response.data
