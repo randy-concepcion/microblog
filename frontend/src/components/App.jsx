7f6ea4e8bdf1a2404650631a02fc1712631d6f73
Randy Concepcion <randy.concepcion@gmail.com>
2023-08-14 19:48:48 -0400

    Create base frontend app

---
+++ frontend/src/components/App.jsx
@@ -0,0 +1,15 @@
+// src/components/App.jsx
+import React from "react";
+import Home from "./Home";
+import Navbar from "./Navbar";
+
+function App() {
+    return (
+        <React.Fragment>
+            <Navbar />
+            <Home />
+        </React.Fragment>
+    );
+}
+
+export default App;
---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,22 @@
+// src/components/Navbar.jsx
+import React from "react";
+
+function Navbar() {
+    return (
+        <div className="w3-bar w3-black">
+            <a className="w3-bar-item w3-button" href="/">
+                Quickr
+            </a>
+            <div style={{ float: "right" }}>
+                <a className="w3-bar-item w3-button" href="/">
+                    Login
+                </a>
+                <a className="w3-bar-item w3-button" href="/">
+                    Register
+                </a>
+            </div>
+        </div>
+    );
+}
+
+export default Navbar;
