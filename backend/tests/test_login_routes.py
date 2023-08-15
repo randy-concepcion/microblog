58093b7383fc118e269362cba92fdc470626a157
Randy Concepcion <randy.concepcion@gmail.com>
2023-08-15 18:54:29 -0400

    Login route: add unit test for null user data returns invalid form response

--- backend/tests/test_login_routes.py
+++ backend/tests/test_login_routes.py
@@ -22,3 +22,18 @@ class TestLoginEndpointPost:
         assert response.status_code == 400
         assert b"error" in response.data
         mock_get_users.assert_not_called()
+
+    def test_null_user_data_returns_invalid_form_response(self):
+        post_json = {"email": "test@test.com", "pwd": None}
+
+        mock_get_users = self.mocker.patch("backend.utils.get_users")
+
+        response = self.test_client.post(
+            self.endpoint,
+            content_type="application/json",
+            json=post_json,
+        )
+
+        assert response.status_code == 400
+        assert b"error" in response.data
+        mock_get_users.assert_not_called()
