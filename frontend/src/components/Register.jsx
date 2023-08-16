2b413b5d74ea6cd438204df2604126ca3d5f0c37
Randy Concepcion <randy.concepcion@gmail.com>
2023-08-16 00:03:15 -0400

    Add frontend code for Login and Register

---
+++ frontend/src/components/Register.jsx
@@ -0,0 +1,49 @@
+// src/components/Register.jsx
+import React, { Component } from "react";
+
+class Register extends Component {
+    render() {
+        return (
+            <div className="w3-card-4" style={{ margin: "2rem" }}>
+                <div className="w3-container w3-blue w3-center w3-xlarge">
+                    REGISTER
+                </div>
+                <div className="w3-container">
+                    <form>
+                        <p>
+                            <label htmlFor="email">Email</label>
+                            <input
+                                type="email"
+                                class="w3-input w3-border"
+                                id="email"
+                            />
+                        </p>
+                        <p>
+                            <label htmlFor="username">Username</label>
+                            <input
+                                type="username"
+                                class="w3-input w3-border"
+                                id="text"
+                            />
+                        </p>
+                        <p>
+                            <label htmlFor="password">Password</label>
+                            <input
+                                type="password"
+                                class="w3-input w3-border"
+                                id="password"
+                            />
+                        </p>
+                        <p>
+                            <button type="submit" class="w3-button w3-blue">
+                                Register
+                            </button>
+                        </p>
+                    </form>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Register;
