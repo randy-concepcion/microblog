ce6911528ef768d1f29482feb100599fd313ae03
Randy Concepcion <randy.concepcion@policygenius.com>
2023-08-28 10:49:20 -0400

    Add Alert function to be used by any component

---
+++ frontend/src/components/Alert.jsx
@@ -0,0 +1,17 @@
+import PropTypes from 'prop-types'
+
+function Alert (props) {
+  return (
+      <div
+          className="w3-pale-red w3-text-red w3-border w3-border-red w3-round-large"
+          style={{ padding: '1rem', marginTop: '1rem' }}>
+          {props.message}
+      </div>
+  )
+}
+
+Alert.propTypes = {
+  message: PropTypes.string
+}
+
+export default Alert
