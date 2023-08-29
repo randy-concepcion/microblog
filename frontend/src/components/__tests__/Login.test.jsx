c1132168a2dfac6d9228da5b3a65ab9c394d8f30
Randy Concepcion <randy.concepcion@policygenius.com>
2023-08-29 00:00:03 -0400

    Add Login test with valid credentials

--- frontend/src/components/__tests__/Login.test.jsx
+++ frontend/src/components/__tests__/Login.test.jsx
@@ -1,6 +1,16 @@
 import { render, screen } from '@testing-library/react'
+import axios from 'axios'
+import userEvent from '@testing-library/user-event'
 import '@testing-library/jest-dom'
 import Login from '../Login'
+
+// TODO: We may want to consider using 'msw' to avoid mocking
+//       specifically axios and use a mock server instead
+//       which allows for refactoring the implementation
+//       without changing these tests
+//       References:
+//       - https://kentcdodds.com/blog/stop-mocking-fetch#then-i-discovered-msw
+//       - https://stackoverflow.com/questions/67101502/how-to-mock-axios-with-jest
 
 test('Login should render Login form component correctly', async () => {
   render(<Login />)
@@ -8,4 +18,30 @@ test('Login should render Login form component correctly', async () => {
   expect(await screen.getByTestId('test-email')).toBeInTheDocument()
   expect(await screen.getByTestId('test-password')).toBeInTheDocument()
   expect(await screen.getByTestId('test-submit-button')).toBeInTheDocument()
+})
+
+test('User logs in with valid credentials', async () => {
+  const mockAxios = jest.spyOn(axios, 'post')
+  mockAxios.mockResolvedValueOnce({
+    ok: true,
+    json: async () => ({ success: true })
+  })
+
+  render(<Login />)
+  const email = screen.getByTestId('test-email')
+  const password = screen.getByTestId('test-password')
+  const submit = screen.getByTestId('test-submit-button')
+
+  await userEvent.type(email, 'test@test.com')
+  await userEvent.type(password, 'test')
+  await userEvent.click(submit)
+
+  expect(mockAxios).toHaveBeenCalledTimes(1)
+  expect(mockAxios).toHaveBeenCalledWith(
+    'http://localhost:5000/api/login',
+    {
+      email: 'test@test.com',
+      pwd: 'test'
+    }
+  )
 })
