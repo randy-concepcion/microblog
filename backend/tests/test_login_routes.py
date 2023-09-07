299c7670b07bac347a9092b8519bd7b21694c9de
Randy Concepcion <randy.concepcion@gmail.com>
2023-09-06 21:09:10 -0400

    Add jwt token to login route + unit tests

--- backend/tests/test_login_routes.py
+++ backend/tests/test_login_routes.py
@@ -23,7 +23,7 @@ class TestLoginEndpointPost:
         assert b"error" in response.data
         mock_get_users.assert_not_called()
 
-    def test_null_user_data_returns_invalid_form_response(self):
+    def test_null_user_data_returns_invalid_credentials(self):
         post_json = {"email": "test@test.com", "pwd": None}
 
         mock_get_users = self.mocker.patch("backend.login.routes.get_users")
@@ -34,15 +34,15 @@ class TestLoginEndpointPost:
             json=post_json,
         )
 
-        assert response.status_code == 400
+        assert response.status_code == 200
         assert b"error" in response.data
         mock_get_users.assert_not_called()
 
     def test_valid_user_data_no_match_returns_false(self):
         post_json = {"email": "test@test.com", "pwd": "12345"}
         users_list = [
-            {"email": "foo@foo.com", "pwd": "12345"},
-            {"email": "bar@bar.com", "pwd": "67890"},
+            {"id": 1, "email": "foo@foo.com", "password": "12345"},
+            {"id": 2, "email": "bar@bar.com", "password": "67890"},
         ]
 
         mock_get_users = self.mocker.patch(
@@ -56,14 +56,14 @@ class TestLoginEndpointPost:
         )
 
         assert response.status_code == 200
-        assert response.json is False
+        assert b"error" in response.data
         mock_get_users.assert_called_once()
 
-    def test_valid_user_data_with_match_returns_true(self):
+    def test_valid_user_data_with_match_returns_success(self):
         post_json = {"email": "test@test.com", "pwd": "12345"}
         users_list = [
-            {"email": post_json["email"], "pwd": post_json["pwd"]},
-            {"email": "bar@bar.com", "pwd": "67890"},
+            {"id": 1, "email": post_json["email"], "password": post_json["pwd"]},
+            {"id": 2, "email": "bar@bar.com", "password": "67890"},
         ]
 
         mock_get_users = self.mocker.patch(
@@ -77,5 +77,5 @@ class TestLoginEndpointPost:
         )
 
         assert response.status_code == 200
-        assert response.json is True
+        assert b"token" in response.data
         mock_get_users.assert_called_once()
