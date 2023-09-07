2b17133454b08a07e98c309af13d5a0bf000600c
Randy Concepcion <randy.concepcion@gmail.com>
2023-09-07 02:30:29 -0400

    Add tests for rendering Home and MainPage components based on valid token

--- frontend/src/components/__tests__/App.test.jsx
+++ frontend/src/components/__tests__/App.test.jsx
@@ -1,8 +1,9 @@
 import { render, screen } from '@testing-library/react'
 import '@testing-library/jest-dom'
 import App from '../App'
+import * as login from '../../utils/login'
 
-test('App should render App fragment component correctly', async () => {
+test('App should render App fragment component correctly for Home page', async () => {
   render(<App />)
 
   expect(await screen.getByTestId('test-navbar')).toBeInTheDocument()
@@ -11,4 +12,26 @@ test('App should render App fragment component correctly', async () => {
   expect(allLinks[0]).toHaveAttribute('href', '/')
   expect(allLinks[1]).toHaveAttribute('href', '/login')
   expect(allLinks[2]).toHaveAttribute('href', '/register')
+
+  // The Home page component is rendered
+  expect(await screen.getByTestId('test-login-button')).toBeInTheDocument()
+  expect(await screen.getByTestId('test-register-button')).toBeInTheDocument()
+  expect(await screen.getByTestId('test-post')).toBeInTheDocument()
+})
+
+test('App should render App fragment component correctly for Main page', async () => {
+  const mockCheckToken = jest.spyOn(login, 'checkToken')
+  mockCheckToken.mockResolvedValueOnce(true)
+  render(<App />)
+
+  expect(await screen.getByTestId('test-navbar')).toBeInTheDocument()
+
+  const allLinks = await screen.getAllByRole('link')
+  expect(allLinks[0]).toHaveAttribute('href', '/')
+  expect(allLinks[1]).toHaveAttribute('href', '/login')
+  expect(allLinks[2]).toHaveAttribute('href', '/register')
+
+  // The Main page component is rendered
+  expect(await screen.getByTestId('test-main-posts')).toBeInTheDocument()
+  expect(await screen.getByTestId('test-posts-list')).toBeInTheDocument()
 })
