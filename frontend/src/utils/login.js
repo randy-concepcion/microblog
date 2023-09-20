5d982c7ca0b08530c18a451176967ff519a0a794
Randy Concepcion <randy.concepcion@gmail.com>
2023-09-20 19:43:48 -0400

    WIP: Add token refresh functionality when checking token

--- frontend/src/utils/login.js
+++ frontend/src/utils/login.js
@@ -9,12 +9,42 @@ async function login (email, pwd) {
     return data.error
   } else {
     localStorage.setItem('token', data.token)
+    localStorage.setItem('refreshToken', data.refreshToken)
     return true
   }
 }
 
-function checkToken () {
-  return localStorage.getItem('token')
+async function checkToken () {
+  const token = localStorage.getItem('token')
+
+  try {
+    const response = await axios.post('/api/token_expiration', {}, {
+      headers: {
+        Authorization: `Bearer ${token}`
+      }
+    })
+
+    const { data } = await response
+    return data.success
+  } catch {
+    console.log('p')
+    const refreshToken = localStorage.getItem('refreshToken')
+
+    if (!refreshToken) {
+      localStorage.removeItem('token')
+      return false
+    }
+
+    axios.post('/api/refresh_token', {}, {
+      headers: {
+        Authorization: `Bearer ${refreshToken}`
+      }
+    }).then(response => {
+      localStorage.setItem('token', response.data.token)
+    })
+
+    return true
+  }
 }
 
 export { login, checkToken }
