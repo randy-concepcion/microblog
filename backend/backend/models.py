579da38c91832ee9abbd4bb5f2892e03b6986fa5
Randy Concepcion <randy.concepcion@gmail.com>
2023-09-20 21:48:31 -0400

    WIP: Add InvalidToken model

--- backend/backend/models.py
+++ backend/backend/models.py
@@ -19,3 +19,18 @@ class Post(db.Model):
     user = db.relationship("User", foreign_keys=uid)
     title = db.Column(db.String(256))
     content = db.Column(db.String(2048))
+
+
+class InvalidToken(db.Model):
+    __tablename__ = "invalid_tokens"
+    id = db.Column(db.Integer, primary_key=True)
+    jti = db.Column(db.String)
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+
+    @classmethod
+    def is_invalid(cls, jti):
+        q = cls.query.filter_by(jti=jti).first()
+        return bool(q)
