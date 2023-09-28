2a51e6177d6b79e456571cf2f4c0c3fc70558d8d
Randy Concepcion <randy.concepcion@gmail.com>
2023-09-27 22:30:40 -0400

    Add AddPost component

---
+++ frontend/src/components/AddPost.jsx
@@ -0,0 +1,24 @@
+// src/components/AddPost.jsx
+
+function AddPost () {
+  return (
+    <div className="w3-modal w3-animate-opacity" id="addPost">
+      <div className="w3-modal-content w3-card">
+        <header className="w3-container w3-blue">
+          <span className="w3-button w3-display-topright w3-hover-none w3-hover-text-white" onClick={ () => {
+            document.getElementById('addPost').style.display = 'none'
+          }}>X</span>
+          <h2>Add Post</h2>
+        </header>
+        <form className="w3-container">
+          <div className="w3-section">
+            <label htmlFor="title">Title</label>
+            <input type="text" id="title" className="w3-input w3-border w3-margin-bottom" />
+            <textarea cols="30" rows="10" />
+          </div>
+        </form>
+      </div>
+    </div>)
+}
+
+export default AddPost
