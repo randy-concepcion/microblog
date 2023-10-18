072435310ab51a1ef0f0593b9ba64a338d7a0e4d
Randy Concepcion <randy.concepcion@gmail.com>
2023-10-17 21:46:22 -0400

    Add delete button to PostItem

--- frontend/src/components/PostItem.jsx
+++ frontend/src/components/PostItem.jsx
@@ -6,11 +6,15 @@ function PostItem (props) {
     <div
       className="w3-card w3-border w3-border-gray w3-round-large"
       style={{ marginTop: '2rem' }}>
+      <header className="w3-container w3-opacity w3-light-gray" style={{ padding: '1rem' }}>@{props.author}</header>
       <div
         className="w3-container"
         style={{ padding: '2rem' }}>
-        <h2 className="w3-opacity w3-xxlarge" data-testid="test-post-title">{props.title}</h2>
-        <div data-testid="test-post-content" dangerouslySetInnerHTML={{ __html: props.content }}></div>
+          <h2 className="w3-xxlarge">
+            <span className="w3-opacity" data-testid="test-post-title">{props.title}</span>
+            <button className="w3-right w3-button w3-red w3-large w3-hover-pale-red w3-round-large">Delete</button>
+          </h2>
+        <div data-testid="test-post-content" dangerouslySetInnerHTML={{ __html: props.content }} />
       </div>
       <footer className="w3-container w3-center w3-large">
         <button className="w3-button" data-testid="test-like-button" style={{ marginRight: '2rem' }}>
@@ -27,7 +31,8 @@ function PostItem (props) {
 
 PostItem.propTypes = {
   title: PropTypes.string,
-  content: PropTypes.string
+  content: PropTypes.string,
+  author: PropTypes.string
 }
 
 export default PostItem
