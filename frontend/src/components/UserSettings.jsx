129943ee0a9d25c70b7beac2c0598a7c703619df
Randy Concepcion <randy-concepcion@users.noreply.github.com>
2023-11-06 01:31:19 -0500

    Add frontend user settings component and unit tests (#44)
    * WIP: Add UserSettings component (need to write tests)
    
    * Add test ids for change password elements
    
    * Add user settings page component tests for change password
    
    * Add component tests for changePassword/backButton + deleteAccount error
    
    * Add component test for successfully deleting account
    
    * Add component test for clicking back from deleteAccount + canceling
    
    * Separate click handler for onKeyDown 'li' elements for accessibility
    
    * Add tests for handling keyboard events for li elements
    
    * Add test/handling for keys pressed for AddPost modal button

---
+++ frontend/src/components/UserSettings.jsx
@@ -0,0 +1,180 @@
+// src/components/UserSettings.jsx
+import React from 'react'
+import Alert from './Alert'
+import axios from 'axios'
+
+class UserSettings extends React.Component {
+  state = {
+    currentSetting: 'main', // values: main, cpwd, del
+    error: ''
+  }
+
+  componentDidMount () {
+    if (!localStorage.getItem('token')) {
+      window.location = '/login'
+    }
+  }
+
+  changePassword = (e) => {
+    e.preventDefault()
+
+    axios.post('/api/change_password',
+      {
+        password: document.getElementById('password').value,
+        newPassword: document.getElementById('npassword').value
+      },
+      {
+        headers: {
+          Authorization: 'Bearer: ' + localStorage.getItem('token')
+        }
+      }
+    ).then(response => {
+      if (response.data.error) {
+        this.setState({ error: response.data.error })
+      } else {
+        alert('Password changed! Logging you out...')
+        window.location = '/logout'
+      }
+    })
+  }
+
+  deleteAccount = (e) => {
+    e.preventDefault()
+
+    const promptChoice = window.confirm('Are you sure you want to delete your account? THIS CANNOT BE UNDONE. ALL OF YOUR POSTS WILL BE DELETED.')
+
+    if (promptChoice) {
+      axios.delete('/api/delete_account',
+        {
+          headers: {
+            Authorization: 'Bearer ' + localStorage.getItem('token')
+          }
+        }
+      ).then(response => {
+        if (response.data.error) {
+          alert('An error occurred: ' + response.data.error)
+        } else {
+          alert('Your account has been deleted. Returning to the home page...')
+          window.location = '/logout'
+        }
+      })
+    }
+  }
+
+  handleClick = (setting) => {
+    this.setState({ currentSetting: setting })
+  }
+
+  render () {
+    return (
+      <div className="w3-container" style={{ margin: '3rem' }}>
+        <div className="w3-card w3-border w3-round-large">
+          <header
+            className="w3-container w3-xlarge w3-blue"
+            style={{ padding: '0.5rem', paddingLeft: '3rem' }}
+          >
+          Settings
+          </header>
+          <div className="w3-container">
+            { this.state.error.length > 0 && <Alert message={ this.state.error } /> }
+            { this.state.currentSetting === 'main' &&
+              <div style={{ margin: '1rem' }}>
+                <h1 className="w3-xxlarge">Settings</h1>
+                <hr className="w3-border-top w3-border-black" />
+                <p>Choose a setting from below:</p>
+                <ul className="w3-ul w3-border w3-hoverable">
+                  <li
+                    onClick={ () => { this.handleClick('cpwd') } }
+                    onKeyDown={ () => { this.handleClick('cpwd') } }
+                    style={{ cursor: 'pointer' }}
+                    className="w3-hover-light-gray"
+                    data-testid='test-change-password'
+                  >
+                  Change password
+                  </li>
+                  <li
+                    onClick={ () => { this.handleClick('del') } }
+                    onKeyDown={ () => { this.handleClick('del') } }
+                    style={{ cursor: 'pointer' }}
+                    className="w3-text-red w3-hover-pale-red w3-hover-text-red"
+                    data-testid='test-delete-account'
+                  >
+                  Delete account
+                  </li>
+                </ul>
+              </div>
+            }
+            {
+              this.state.currentSetting === 'cpwd' &&
+              <div style={{ margin: '1rem' }}>
+                <h1 className="w3-xxlarge">Change password</h1>
+                <hr className="w3-border-top w3-border-black" />
+                <button
+                  data-testid='test-back-button'
+                  className="w3-button w3-blue"
+                  onClick={ () => { this.handleClick('main') } }
+                >
+                &laquo; Back
+                </button>
+                <form onSubmit={ this.changePassword }>
+                  <p>
+                    <label htmlFor="password">Old password</label>
+                    <input
+                      type="password"
+                      id="password"
+                      className="w3-input w3-border"
+                      data-testid="test-old-password"
+                    />
+                  </p>
+                  <p>
+                    <label htmlFor="npassword">New password</label>
+                    <input
+                      type="password"
+                      id="npassword"
+                      className="w3-input w3-border"
+                      data-testid="test-new-password"
+                    />
+                  </p>
+                  <p>
+                    <button
+                      type="submit"
+                      className="w3-button w3-blue"
+                      data-testid="test-submit-button"
+                    >
+                    Submit
+                    </button>
+                  </p>
+                </form>
+              </div>
+            }
+            {
+              this.state.currentSetting === 'del' &&
+              <div style={{ margin: '1rem' }}>
+                <h1 className="w3-xxlarge">Delete account</h1>
+                <hr className="w3-border-top w3-border-black" />
+                <button
+                  className="w3-button w3-blue"
+                  data-testid="test-back-button"
+                  onClick={ () => { this.handleClick('main') } }
+                >
+                &laquo; Back
+                </button>
+                <p>
+                  <button
+                    className="w3-button w3-red w3-large"
+                    data-testid="test-delete-account-button"
+                    onClick={ this.deleteAccount }
+                  >
+                  DELETE ACCOUNT
+                  </button>
+                </p>
+              </div>
+            }
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default UserSettings
---
+++ frontend/src/components/__tests__/UserSettings.test.jsx
@@ -0,0 +1,212 @@
+import { fireEvent, render, screen } from '@testing-library/react'
+import axios from 'axios'
+import userEvent from '@testing-library/user-event'
+import '@testing-library/jest-dom'
+import UserSettings from '../UserSettings'
+
+describe('UserSettings: no token is stored in localStorage', () => {
+  beforeEach(() => {
+    jest.clearAllMocks()
+    delete window.location
+  })
+
+  test('User returns to login page', () => {
+    render(<UserSettings />)
+    expect(window.location).toBe('/login')
+  })
+})
+
+describe('UserSettings: User attempts to change password', () => {
+  beforeEach(() => {
+    jest.clearAllMocks()
+    delete window.location
+  })
+
+  test('User encounters an error when changing password', async () => {
+    const mockToken = 'mock-auth-token'
+
+    localStorage.setItem('token', mockToken)
+
+    axios.post = jest.fn()
+    axios.post.mockResolvedValueOnce({
+      data: {
+        error: 'Error: Unable to change password'
+      }
+    })
+
+    render(<UserSettings />)
+    const changePassword = screen.getByTestId('test-change-password')
+    await userEvent.click(changePassword)
+
+    const submitButton = screen.getByTestId('test-submit-button')
+    await userEvent.click(submitButton)
+
+    expect(await screen.getByTestId('test-alert-msg')).toBeInTheDocument()
+  })
+
+  test('User successfully changes password', async () => {
+    const mockToken = 'mock-auth-token'
+
+    localStorage.setItem('token', mockToken)
+
+    axios.post = jest.fn()
+    axios.post.mockResolvedValueOnce({
+      data: {
+        success: true
+      }
+    })
+
+    window.alert = jest.fn()
+
+    render(<UserSettings />)
+    const changePassword = screen.getByTestId('test-change-password')
+    await userEvent.click(changePassword)
+
+    const oldPassword = screen.getByTestId('test-old-password')
+    await userEvent.type(oldPassword, 'old password')
+
+    const newPassword = screen.getByTestId('test-new-password')
+    await userEvent.type(newPassword, 'new password')
+
+    const submitButton = screen.getByTestId('test-submit-button')
+    await userEvent.click(submitButton)
+
+    expect(window.location).toBe('/logout')
+  })
+
+  test('User clicks back button to return to main page', async () => {
+    const mockToken = 'mock-auth-token'
+
+    localStorage.setItem('token', mockToken)
+
+    render(<UserSettings />)
+    const changePassword = screen.getByTestId('test-change-password')
+    await userEvent.click(changePassword)
+
+    const backBtn = screen.getByTestId('test-back-button')
+    await userEvent.click(backBtn)
+
+    expect(await screen.getByTestId('test-change-password')).toBeInTheDocument()
+    expect(await screen.getByTestId('test-delete-account')).toBeInTheDocument()
+  })
+
+  test('(a11y) User uses keyboard to access change password form', async () => {
+    const mockToken = 'mock-auth-token'
+
+    localStorage.setItem('token', mockToken)
+
+    render(<UserSettings />)
+    const changePassword = screen.getByTestId('test-change-password')
+    await fireEvent.keyDown(changePassword, { key: 'Enter', code: 'Enter', charCode: 13 })
+
+    expect(await screen.getByTestId('test-old-password')).toBeInTheDocument()
+    expect(await screen.getByTestId('test-new-password')).toBeInTheDocument()
+    expect(await screen.getByTestId('test-submit-button')).toBeInTheDocument()
+  })
+})
+
+describe('UserSettings: User attempts to delete account', () => {
+  beforeEach(() => {
+    jest.clearAllMocks()
+    delete window.location
+    delete window.alert
+    delete window.confirm
+  })
+
+  test('User encounters an error when deleting account', async () => {
+    const mockToken = 'mock-auth-token'
+
+    localStorage.setItem('token', mockToken)
+
+    axios.delete = jest.fn()
+    axios.delete.mockResolvedValueOnce({
+      data: {
+        error: 'Error: Unable to delete account'
+      }
+    })
+
+    window.alert = jest.fn()
+    window.confirm = jest.fn(() => true)
+
+    render(<UserSettings />)
+    const deleteAccount = screen.getByTestId('test-delete-account')
+    await userEvent.click(deleteAccount)
+
+    const deleteAccountBtn = screen.getByTestId('test-delete-account-button')
+    await userEvent.click(deleteAccountBtn)
+
+    expect(window.alert).toHaveBeenCalled()
+  })
+
+  test('User successfully deletes account', async () => {
+    const mockToken = 'mock-auth-token'
+
+    localStorage.setItem('token', mockToken)
+
+    axios.delete = jest.fn()
+    axios.delete.mockResolvedValueOnce({
+      data: {
+        success: true
+      }
+    })
+
+    window.alert = jest.fn()
+    window.confirm = jest.fn(() => true)
+
+    render(<UserSettings />)
+    const deleteAccount = screen.getByTestId('test-delete-account')
+    await userEvent.click(deleteAccount)
+
+    const deleteAccountBtn = screen.getByTestId('test-delete-account-button')
+    await userEvent.click(deleteAccountBtn)
+
+    expect(window.location).toBe('/logout')
+  })
+
+  test('User clicks back button to return to main page', async () => {
+    const mockToken = 'mock-auth-token'
+
+    localStorage.setItem('token', mockToken)
+
+    render(<UserSettings />)
+    const deleteAccount = screen.getByTestId('test-delete-account')
+    await userEvent.click(deleteAccount)
+
+    const backBtn = screen.getByTestId('test-back-button')
+    await userEvent.click(backBtn)
+
+    expect(await screen.getByTestId('test-change-password')).toBeInTheDocument()
+    expect(await screen.getByTestId('test-delete-account')).toBeInTheDocument()
+  })
+
+  test('User cancels deleting account', async () => {
+    const mockToken = 'mock-auth-token'
+
+    localStorage.setItem('token', mockToken)
+
+    axios.delete = jest.fn()
+
+    window.confirm = jest.fn(() => false)
+
+    render(<UserSettings />)
+    const deleteAccount = screen.getByTestId('test-delete-account')
+    await userEvent.click(deleteAccount)
+
+    const deleteAccountBtn = screen.getByTestId('test-delete-account-button')
+    await userEvent.click(deleteAccountBtn)
+
+    expect(axios.delete).not.toHaveBeenCalled()
+  })
+
+  test('(a11y) User uses keyboard to access account deletion', async () => {
+    const mockToken = 'mock-auth-token'
+
+    localStorage.setItem('token', mockToken)
+
+    render(<UserSettings />)
+    const deleteAccount = screen.getByTestId('test-delete-account')
+    await fireEvent.keyDown(deleteAccount, { key: 'Enter', code: 'Enter', charCode: 13 })
+
+    expect(await screen.getByTestId('test-delete-account-button')).toBeInTheDocument()
+  })
+})
